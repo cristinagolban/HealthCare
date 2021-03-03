a475e0a812b0c6c60a523937a934fcc02b780aef
SofranSebastian <sofransebastian@yahoo.com>
2021-03-03 22:51:48 +0200

    am adaugat fonturi noi

--- App.js
+++ App.js
@@ -27,28 +27,45 @@ import "firebase/storage";
   firebase.initializeApp(firebaseConfig);
  
   //Imports for the fonts 
-  //import * as Font from 'expo-font';
-  //import  AppLoading from 'expo-app-loading';
+  import * as Font from 'expo-font';
+  import  AppLoading from 'expo-app-loading';
  
   import MyStack from './routes/routes.js';
- 
+
+  const customFonts = {
+    'normal-font': require('./fonts/Helvetica-Normal.ttf'),
+    'light-font': require('./fonts/Helvetica-Light.ttf'),
+    'bold-font': require('./fonts/Helvetica-Bold.ttf')
+  };
+
   export default class App extends React.Component{
- 
+
     constructor(){
- 
+  
       super();
- 
+  
       this.state = {
-        
+        fontsLoaded: false
       }
     }
- 
   
- 
-    render(){
-        return(
-          <MyStack />
-        )
+    async _loadFontsAsync(){
+      await Font.loadAsync(customFonts);
+      this.setState({fontsLoaded:true})
     }
- 
+  
+    async componentDidMount(){
+      this._loadFontsAsync();
+    }
+  
+    render(){
+        if(this.state.fontsLoaded){
+            return(
+              <MyStack />
+            )
+        }else{
+            return <AppLoading/>
+        }  
+    }
+  
   }
